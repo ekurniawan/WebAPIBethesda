6b1a798af5da94dbfdd6f68c41ad7b2898c3e72d
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-09-14 16:37:14 +0700

    menambahkan kategori by id

---
+++ SampleBackendAPI/Controllers/KategoriController.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Net
+Imports System.Web.Http
+
+Namespace Controllers
+    Public Class KategoriController
+        Inherits ApiController
+
+        ' GET: api/Kategori
+        Public Function GetValues() As IEnumerable(Of Kategori)
+            Dim kategoriDal As New KategoriDAL
+            Return kategoriDal.GetAll()
+        End Function
+
+        ' GET: api/Kategori/5
+        Public Function GetValue(ByVal id As Integer) As Kategori
+            Dim kategoriDal As New KategoriDAL
+            Return kategoriDal.GetById(id)
+        End Function
+
+        ' POST: api/Kategori
+        Public Sub PostValue(<FromBody()> ByVal value As String)
+
+        End Sub
+
+        ' PUT: api/Kategori/5
+        Public Sub PutValue(ByVal id As Integer, <FromBody()> ByVal value As String)
+
+        End Sub
+
+        ' DELETE: api/Kategori/5
+        Public Sub DeleteValue(ByVal id As Integer)
+
+        End Sub
+    End Class
+End Namespace
--- SampleBackendAPI/DAL/KategoriDAL.vb
+++ SampleBackendAPI/DAL/KategoriDAL.vb
@@ -20,7 +20,7 @@ Public Class KategoriDAL
             If dr.HasRows Then
                 While dr.Read()
                     Dim kat As New Kategori With {
-                        .KategoriId = CInt(dr("KategoriId")), .NamaKategori = dr("NamaKategori")
+                        .KategoriId = CInt(dr("KategoriId")), .NamaKategori = dr("NamaKategori").ToString
                     }
                     lstKategori.Add(kat)
                 End While
@@ -30,6 +30,29 @@ Public Class KategoriDAL
             conn.Close()
 
             Return lstKategori
+        End Using
+    End Function
+
+    Public Function GetById(id As Integer) As Kategori
+        Using conn As New SqlConnection(GetConnStr())
+            Dim kat As New Kategori
+
+            Dim strSql = "select * from Kategori where KategoriId=@KategoriId"
+            cmd = New SqlCommand(strSql, conn)
+            cmd.Parameters.AddWithValue("@KategoriId", id)
+
+            Dim dr As SqlDataReader = cmd.ExecuteReader()
+            If dr.HasRows Then
+                While dr.Read()
+                    kat.KategoriId = CInt(dr("KategoriId"))
+                    kat.NamaKategori = dr("NamaKategori").ToString
+                End While
+            End If
+            dr.Close()
+            cmd.Dispose()
+            conn.Close()
+
+            Return kat
         End Using
     End Function
 
